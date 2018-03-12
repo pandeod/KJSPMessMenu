d7daab3b55fe86fc7776129c3215cb37ed984a12
pandeod <onkardpande@gmail.com>
2018-03-13 00:24:17 +0530

    about activity added

---
+++ app/src/main/java/com/example/onkarpande/kjspmessmenu/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.example.onkarpande.kjspmessmenu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+    }
+}
--- app/src/main/java/com/example/onkarpande/kjspmessmenu/MainActivity.java
+++ app/src/main/java/com/example/onkarpande/kjspmessmenu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.onkarpande.kjspmessmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -154,7 +155,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent i = new Intent(this,AboutActivity.class);
+            startActivity(i);
         }
 
         return super.onOptionsItemSelected(item);
